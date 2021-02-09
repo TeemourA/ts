5b0bbdac20d7cb45d56d42d6ae2edcf8e0f2243f
TeemourA <taxprogramming@gmail.com>
2021-02-09 21:04:56 +0300

    add union type to func

--- app.ts
+++ app.ts
@@ -1,10 +1,24 @@
-const combine = (input1: number | string, input2: number | string) => {
-  const result = input1 + input2;
-  return result;
+const combine = (
+  input1: number | string,
+  input2: number | string,
+  resultConvertion: 'as-number' | 'as-text'
+) => {
+  let result;
+  if (typeof input1 === 'number' && typeof input2 === 'number') {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+
+  if (resultConvertion === 'as-number') {
+    return Number(result);
+  } else {
+    return result;
+  }
 };
 
-const combinedAges = combine(36, 20);
+const combinedAges = combine(36, 20, 'as-number');
+const combinedNames = combine('T', 'I', 'as-text');
+const combinedStringAges = combine('36', '20', 'as-text');
 
-console.log(combinedAges);
-
-const combinedNames = combine('T', 'I');
+console.log(combinedAges, combinedNames, combinedStringAges);
